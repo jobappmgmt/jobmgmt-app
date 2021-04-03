2cf56f17039af4533061657f04013245cf329025
jobappmgmt <jobappmgmt@gmail.com>
2021-04-03 20:56:13 -0300

    Revert "teste final para a v1"
    This reverts commit 8d2178aae0ae53cbf498a714277607b0d90ce47c.

--- src/App.js
+++ src/App.js
@@ -1,125 +1,25 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import { BrowserRouter as Router, Switch } from "react-router-dom"
-import { connect } from "react-redux"
+import logo from './logo.svg';
+import './App.css';
 
-// Import Routes
-import { authProtectedRoutes, publicRoutes } from "./routes/"
-import AppRoute from "./routes/route"
-
-// layouts
-import VerticalLayout from "./components/VerticalLayout/"
-import HorizontalLayout from "./components/HorizontalLayout/"
-import NonAuthLayout from "./components/NonAuthLayout"
-
-// Import scss
-import "./assets/scss/theme.scss"
-
-// Import Firebase Configuration file
-// import { initFirebaseBackend } from "./helpers/firebase_helper"
-
-// Import fackbackend Configuration file
-import fakeBackend from "./helpers/AuthType/fakeBackend"
-
-// Activating fake backend
-fakeBackend()
-
-// Activating fake firebase
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_APIKEY,
-//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASEURL,
-//   projectId: process.env.REACT_APP_PROJECTID,
-//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-//   appId: process.env.REACT_APP_APPID,
-//   measurementId: process.env.REACT_APP_MEASUREMENTID,
-// };
-
-// init firebase backend
-// initFirebaseBackend(firebaseConfig);
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-    this.getLayout = this.getLayout.bind(this)
-  }
-
-  /**
-   * Returns the layout
-   */
-  getLayout = () => {
-    let layoutCls = VerticalLayout
-
-    switch (this.props.layout.layoutType) {
-      case "horizontal":
-        layoutCls = HorizontalLayout
-        break
-      default:
-        layoutCls = VerticalLayout
-        break
-    }
-    return layoutCls
-  }
-
-  // onRoutechange() {
-  //   alert("hii")
-  //   setTimeout(() => {
-  //     if (document.getElementsByClassName("mm-active").length > 0) {
-  //       console.log(ref.current.el);
-  //       const currentPosition = document.getElementsByClassName("mm-active")[0]
-  //         .offsetTop
-  //         console.log(currentPosition)
-  //       if (currentPosition > 500)
-  //         this.$refs.currentMenu.SimpleBar.getScrollElement().scrollTop =
-  //           currentPosition + 300
-  //     }
-  //   }, 300)
-  // }
-
-  render() {
-    const Layout = this.getLayout()
-
-    return (
-      <React.Fragment>
-        <Router>
-          <Switch>
-            {publicRoutes.map((route, idx) => (
-              <AppRoute
-                path={route.path}
-                layout={NonAuthLayout}
-                component={route.component}
-                key={idx}
-                isAuthProtected={false}
-              />
-            ))}
-
-            {authProtectedRoutes.map((route, idx) => (
-              <AppRoute
-                path={route.path}
-                layout={Layout}
-                component={route.component}
-                key={idx}
-                isAuthProtected={true}
-                exact
-              />
-            ))}
-          </Switch>
-        </Router>
-      </React.Fragment>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    layout: state.Layout,
-  }
-}
-
-App.propTypes = {
-  layout: PropTypes.object,
-}
-
-export default connect(mapStateToProps, null)(App)
+export default App;
