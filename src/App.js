798bb48759fd254b4230aac547ab639fb9d87b5d
jobappmgmt <jobappmgmt@gmail.com>
2021-04-03 21:12:53 -0300

    adicionar components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { BrowserRouter as Router, Switch } from "react-router-dom"
+import { connect } from "react-redux"
+
+// Import Routes
+import { authProtectedRoutes, publicRoutes } from "./routes/"
+import AppRoute from "./routes/route"
+
+// layouts
+import VerticalLayout from "./components/VerticalLayout/"
+import HorizontalLayout from "./components/HorizontalLayout/"
+import NonAuthLayout from "./components/NonAuthLayout"
+
+// Import scss
+import "./assets/scss/theme.scss"
+
+// Import Firebase Configuration file
+// import { initFirebaseBackend } from "./helpers/firebase_helper"
+
+// Import fackbackend Configuration file
+import fakeBackend from "./helpers/AuthType/fakeBackend"
+
+// Activating fake backend
+fakeBackend()
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.getLayout = this.getLayout.bind(this)
+  }
+
+   /**
+   * Returns the layout
+   */
+    getLayout = () => {
+      let layoutCls = VerticalLayout
+  
+      switch (this.props.layout.layoutType) {
+        case "horizontal":
+          layoutCls = HorizontalLayout
+          break
+        default:
+          layoutCls = VerticalLayout
+          break
+      }
+      return layoutCls
+    }
+
+    render() {
+      const Layout = this.getLayout()
+      return (
+        <React.Fragment>
+        <Router>
+          <Switch>
+            {publicRoutes.map((route, idx) => (
+              <AppRoute
+                path={route.path}
+                layout={NonAuthLayout}
+                component={route.component}
+                key={idx}
+                isAuthProtected={false}
+              />
+            ))}
+
+            {authProtectedRoutes.map((route, idx) => (
+              <AppRoute
+                path={route.path}
+                layout={Layout}
+                component={route.component}
+                key={idx}
+                isAuthProtected={true}
+                exact
+              />
+            ))}
+          </Switch>
+        </Router>
+      </React.Fragment>
+      )
+    } 
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    layout: state.Layout,
+  }
+}
+
+App.propTypes = {
+  layout: PropTypes.object,
+}
+
+export default connect(mapStateToProps, null)(App)
