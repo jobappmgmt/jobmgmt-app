7774a98de3809720c0bbc1c4872c5dab8f96f634
jobappmgmt <jobappmgmt@gmail.com>
2021-04-04 21:07:51 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import React, { Component } from "react"
-import PropTypes from "prop-types"
-import { BrowserRouter as Router, Switch } from "react-router-dom"
-import { connect } from "react-redux"
 
-// Import Routes
-import { authProtectedRoutes, publicRoutes } from "./routes/"
-import AppRoute from "./routes/route"
-
-// layouts
-import VerticalLayout from "./components/VerticalLayout/"
-import HorizontalLayout from "./components/HorizontalLayout/"
-import NonAuthLayout from "./components/NonAuthLayout"
-
-// Import scss
-import "./assets/scss/theme.scss"
-
-// Import Firebase Configuration file
-// import { initFirebaseBackend } from "./helpers/firebase_helper"
-
-// Import fackbackend Configuration file
-import fakeBackend from "./helpers/AuthType/fakeBackend"
 
 // Activating fake backend
 fakeBackend()
